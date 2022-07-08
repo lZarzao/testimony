4e9465e01a498cdf3d3757dba9c420e45b6641b9
lZarzao <luiszarzao@icloud.com>
2022-07-07 23:24:13 -0400

    reusing component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,22 @@ function App() {
           company='Amazon'
           testimony={`It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.`}
         />
+        <Testimony
+          name='Sarah Chima'
+          country='Nigeria'
+          img='sarah.png'
+          job='Software Engineer'
+          company='ChatDesk'
+          testimony={`freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.`}
+        />
+        <Testimony
+          name='Emma Bostian'
+          country='Sweden'
+          img='emma.png'
+          job='Software Engineer'
+          company='Spotify'
+          testimony={`I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.`}
+        />
       </div>
     </div>
   );
